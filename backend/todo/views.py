af519f3822e159901f9980289669a4722722d50b
joelproxi <joeledmond95@gmail.com>
2021-04-14 00:00:16 +0100

    first commit

---
+++ backend/todo/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+
+from .models import Todo
+from .serializers import TodoSerializer
+
+class ListTodoView(ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class CreateTodoView(CreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class UpdateTodoView(UpdateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DeleteTodoView(DestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
